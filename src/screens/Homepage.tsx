113326e9a71603040832a4e2fb6e7149e926b341
ankh6 <andynzemokalumu@live.be>
2022-01-24 21:01:36 +0100

    Added components backbone

--- src/screens/Homepage.tsx
+++ src/screens/Homepage.tsx
@@ -1,7 +1,49 @@
-export const Homepage = () => {
+export function Homepage(props : any) {
     return (
-        <h1>
-            This is the homepage then.
-        </h1>
+        <div>
+            <h1>Welcome to the {props.name} !</h1>
+            {props.children}
+        </div>
+    );  
+}
+
+export function SearchBar(){
+    return (
+        <div>
+            <h1>This is the SearchBar!</h1>
+        </div>
+    )
+}
+
+export function Chat(){
+    return (
+        <div>
+            <h1>This is the Chat!</h1>
+        </div>
+    )
+}
+
+export function DaoTable(props: any){
+    return (
+        <DaoTable>
+            <DaoHeader/>
+            <DaoList/>
+        </DaoTable>
+    )
+}
+
+function DaoHeader(){
+    return (
+        <div>
+            <h1>This is the Dao Header!</h1>
+        </div>
+    )
+}
+
+function DaoList(){
+    return (
+        <div>
+            <h1>This is the DaoList!</h1>
+        </div>
     )
 }
