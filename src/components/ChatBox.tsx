394a833574628d1e33cd925ea6c8649e2699c447
Justin <jusheinz@aol.de>
2022-01-27 00:19:15 +0000

    first version sidebar and chat box

---
+++ src/components/ChatBox.tsx
@@ -0,0 +1,40 @@
+import 'react-chatbox-component/dist/style.css';
+import { ChatBox } from 'react-chatbox-component';
+import { useEffect, useState } from 'react';
+import { addDoc, collection, getFirestore, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { firebaseApp } from '../util/firebaseConnection';
+import { message } from '../util/types';
+import { useMoralis } from 'react-moralis';
+import { MessageSharp } from '@mui/icons-material';
+export const Chat = () => {
+    const [messages, setMessages] = useState([] as message[])
+    const { Moralis } = useMoralis()
+    useEffect(() => {
+        return onSnapshot(query(collection(getFirestore(firebaseApp), "messages"), orderBy("id", "asc")), (query) => {
+            let messages = [] as message[]
+            query.forEach((doc) => messages.push(doc.data() as message))
+            setMessages(messages)
+        })
+    }, [])
+    return (
+        <ChatBox
+            messages={messages}
+            user={{
+                "uri": Moralis.account,
+                "name": Moralis.account,
+                "avatar": "https://data.cometchat.com/assets/images/avatars/ironman.png",
+            }}
+            onSubmit={(text: string) => {
+                addDoc(collection(getFirestore(firebaseApp), "messages"), {
+                    text: text,
+                    id: messages.length + 1,
+                    sender: {
+                        "uri": Moralis.account,
+                        "name": Moralis.account,
+                        "avatar": "https://data.cometchat.com/assets/images/avatars/ironman.png",
+                    }
+                })
+            }}
+        />
+    )
+}
