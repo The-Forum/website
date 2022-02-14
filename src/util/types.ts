da7b44ecfec851819c2dd20b9e27df6a30863d60
ankh6 <andynzemokalumu@live.be>
2022-02-14 15:52:49 +0100

    Removed unsupported preferences

--- src/util/types.ts
+++ src/util/types.ts
@@ -5,25 +5,16 @@ import { createContext } from "react";
 export const preferences = [
   "Cryptocurrencies",
   "Investing",
-  "Start-Ups",
   "Venture Capital",
   "Sports",
   "Gaming",
   "Culture",
   "Art",
   "Food",
-  "Social Media",
   "Community",
-  "Feminism",
-  "Person of Color",
-  "Social Justice",
   "Climate Change",
-  "Education",
-  "Innovation",
-  "Software",
-  "Technology",
   "Blockchain",
-  "Web3.0",
+  "Web3.0"
 ];
 export const userMenuItems = [
   {
